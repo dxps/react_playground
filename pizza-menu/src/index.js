21a906ac56ef9609f92aa6f8ecefabffdc4949c9
dxps <marius.ileana@gmail.com>
2023-11-25 14:18:47 +0200

    feat(pizza-menu): conditional rendering
    Using && and ternary operator.

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -68,15 +68,20 @@ const Header = () => {
 };
 
 const Menu = () => {
+  const havePizzas = pizzaData.length > 0;
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {havePizzas ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later.</p>
+      )}
     </main>
   );
 };
@@ -90,7 +95,16 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} | We're currently open!
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
 };
