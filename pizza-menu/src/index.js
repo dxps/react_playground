4e5dc5c0403f061c55228e959fbc786495259935
dxps <marius.ileana@gmail.com>
2023-12-06 08:35:50 +0200

    refact(pizza-menu): destructuring props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -96,7 +96,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order openHour={openHour} closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -106,26 +106,27 @@ const Footer = () => {
   );
 };
 
-function Order(props) {
+function Order({ openHour, closeHour }) {
   return (
     <div className="order">
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.
+        We're open from {openHour}:00 until {closeHour}:00. Come visit us or
+        order online.
       </p>
       <button className="btn">Order</button>
     </div>
   );
 }
 
-function Pizza(props) {
-  console.log("Pizza props:", props);
+function Pizza({ pizzaObj }) {
+  console.log("[Pizza] pizzaObj:", pizzaObj);
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
