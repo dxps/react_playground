04e8de8e0291d35296b840f8ac7ded6195b8ec9f
dxps <marius.ileana@gmail.com>
2023-12-12 09:33:42 +0200

    feat(travel-list): derived state

--- ultimate_react_course_2024/travel-list/src/App.js
+++ ultimate_react_course_2024/travel-list/src/App.js
@@ -26,7 +26,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -43,7 +43,6 @@ function Form({ onAddItems }) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.info("newItem=", newItem);
     onAddItems(newItem);
     setDescription("");
     setQuantity(1);
@@ -106,10 +105,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className="stats">Start adding some items to your packing list. 🚀</p>
+    );
+  }
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list. You already packed X (X%).</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go!"
+          : `You have ${numItems} items on your list. You already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
