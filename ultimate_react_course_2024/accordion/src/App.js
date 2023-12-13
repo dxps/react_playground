b8136d07a01e404a6f2e19d3568e07f71fb1e178
dxps <marius.ileana@gmail.com>
2023-12-13 09:38:12 +0200

    feat(accordion): one item open
    Thus, lifting the state of the current open (currOpen) up in the App.

--- ultimate_react_course_2024/accordion/src/App.js
+++ ultimate_react_course_2024/accordion/src/App.js
@@ -2,49 +2,62 @@ import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
-    {
-        title: "Where are these chairs assembled?",
-        text:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
-    },
-    {
-        title: "How long do I have to return my chair?",
-        text:
-            "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
-    },
-    {
-        title: "Do you ship to countries outside the EU?",
-        text:
-            "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-    }
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
 ];
 
 export default function App() {
-    return (
-        <div>
-            <Accordion data={faqs} />
-        </div>
-    );
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
 
 function Accordion({ data }) {
-    return <div className="accordion">
-        {data.map((el, i) => <AccordionItem num={i} title={el.title} text={el.text} key={el.title} />)}
-    </div>;
+  const [currOpen, setCurrOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          currOpen={currOpen}
+          onOpen={setCurrOpen}
+          num={i}
+          title={el.title}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
 }
 
-function AccordionItem({ num, title, text }) {
-    const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ currOpen, onOpen, num, title, children }) {
+  const isOpen = num === currOpen;
 
-    function handleToggle() {
-        setIsOpen(isOpen => !isOpen);
-    }
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
 
-    return <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
-        <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
-        <p className="text">{title}</p>
-        <p className="icon"> {isOpen ? '-' : '+'}</p>
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num}</p>
+      <p className="text">{title}</p>
+      <p className="icon"> {isOpen ? "-" : "+"}</p>
 
-        {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
+  );
 }
