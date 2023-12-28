232d4a7048867e203b8c31eddde413cae1a2962a
dxps <marius.ileana@gmail.com>
2023-12-28 23:10:57 +0200

    react-mui-practice added

---
+++ react-mui-practice/src/Test.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material'
+
+function Test() {
+	return (
+		<>
+			<Typography variant="h6" sx={{ color: 'green' }}>
+				Test Component
+			</Typography>
+		</>
+	)
+}
+
+export default Test
