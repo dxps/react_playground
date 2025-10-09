bd13a111b0472e930958398828fcaa1e4dd298f4
dxps <marius.ileana@gmail.com>
2025-10-09 17:40:59 +0300

    next15-ecommerce: link to product page

--- next15-ecommerce/app/ProductCard.tsx
+++ next15-ecommerce/app/ProductCard.tsx
@@ -8,28 +8,31 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card'
+import Link from 'next/link'
 
 export function ProductCard({ product }: { product: Product }) {
 	return (
-		<Card className="pt-0 overflow-hidden">
-			<div className="relative aspect-video">
-				{product.image && (
-					<Image
-						src={product.image}
-						alt={product.name}
-						fill
-						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-						className="object-cover"
-					/>
-				)}
-			</div>
-			<CardHeader>
-				<CardTitle>{product.name}</CardTitle>
-				<CardDescription>{product.description}</CardDescription>
-			</CardHeader>
-			<CardFooter>
-				<p>{formatPrice(product.price)}</p>
-			</CardFooter>
-		</Card>
+		<Link href={`/products/${product.slug}`}>
+			<Card className="pt-0 overflow-hidden">
+				<div className="relative aspect-video">
+					{product.image && (
+						<Image
+							src={product.image}
+							alt={product.name}
+							fill
+							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							className="object-cover"
+						/>
+					)}
+				</div>
+				<CardHeader>
+					<CardTitle>{product.name}</CardTitle>
+					<CardDescription>{product.description}</CardDescription>
+				</CardHeader>
+				<CardFooter>
+					<p>{formatPrice(product.price)}</p>
+				</CardFooter>
+			</Card>
+		</Link>
 	)
 }
